550a79934b776d812e3c2102994874ed0a2958eb
pandasjw <814201171@qq.com>
2016-09-25 11:05:48 +0800

    运用gulp生成建议sprite

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var spritesmith = require('gulp.spritesmith');
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('src/images/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css'
+  }));
+  return spriteData.pipe(gulp.dest('output/'));
+});
