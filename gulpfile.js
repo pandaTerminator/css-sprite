8bff1e31b4faca64a21f9bba6ec04e0e11e51d34
pandasjw <814201171@qq.com>
2016-10-09 21:31:17 +0800

    增加一个动态相对路径地址

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var spritesmith = require('gulp.spritesmith');
 gulp.task('sprite', function () {
   var spriteData = gulp.src('src/images/*.png').pipe(spritesmith({
     imgName: 'sprite.png',
-    cssName: 'sprite.css'
+    cssName: 'sprite.css',
+    imgPath: '../../sprite.png'
   }));
   return spriteData.pipe(gulp.dest('output/'));
 });
